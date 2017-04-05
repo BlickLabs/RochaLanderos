9720830707150bb59382fac1fc0142467f9a22e0
Alan Sanchez <sanchezpineda03@gmail.com>
2017-04-04 22:36:41 -0500

    #4. Added new carousel.

--- src/js/thefirm_careers.js
+++ src/js/thefirm_careers.js
@@ -1,26 +1,38 @@
 (function () {
-  var carousel = null;
+  var lawyersCarousel = null,
+    photosCarousel = null;
 
-  function createCarousel() {
-    if (carousel === null) {
-      carousel = $('#lawyers-carousel').slick({
+  function createLawyersCarousel() {
+    if (lawyersCarousel === null) {
+      lawyersCarousel = $('#lawyers-carousel').slick({
         prevArrow: '<span class="slick-prev fa fa-angle-left"></span>',
         nextArrow: '<span class="slick-next fa fa-angle-right"></span>'
       });
     }
   }
 
+  function createImagesCarousel() {
+    if (photosCarousel === null) {
+      photosCarousel = $('#photos-carousel').slick({
+        prevArrow: '<span class="slick-prev fa fa-angle-left"></span>',
+        nextArrow: '<span class="slick-next fa fa-angle-right"></span>'
+      });
+    }
+  }
+  createImagesCarousel();
+
   $('#careers-top-button').click(function () {
     $('#careers-top').slideUp(300);
     $('#careers-form').slideDown(300);
+    $(window).scrollTop(0);
   });
 
   $('.lawyers__item .site-button').click(function () {
     var index = parseInt($(this).parent().index());
     $('#lawyers-list-div').slideUp(300);
     $('#lawyers-carousel-div').slideDown(300);
-    createCarousel();
-    carousel.slick('slickGoTo', index);
+    createLawyersCarousel();
+    lawyersCarousel.slick('slickGoTo', index);
     $(window).scrollTop(0);
   });
 
